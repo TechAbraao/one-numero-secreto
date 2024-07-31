a22606526f0ed2d0eaf587bc2a5df7b52283a183
Abraão Santos <115851871+TechAbraao@users.noreply.github.com>
2024-07-31 20:15:32 -0300

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -87,7 +87,7 @@ export default function Home() {
    return (
       <>
          <section className="w-full h-screen flex justify-center items-center bg-gradient-to-r from-green-500 to-emerald-500">
-            <div className="h-2/3 w-1/2 flex-col flex items-center justify-center rounded-3xl shadow-xl bg-corCard border-4 border-solid border-white-100 s380:w-82 s430:w-96 s530:w-106 s830:w-116 s1030:w-126">
+            <div className="h-2/3 flex-col flex items-center justify-center shadow-xl bg-corCard border-4 border-solid border-white-100 s380:w-82 s430:w-96 s530:w-106 s830:w-116 s1030:w-1/2">
                <h1 className="mb-5 text-green-500 font-semibold text-center s380:text-xl s430:text-2xl s630:text-3xl s930:text-2 s1030:text-4xl">
                   {iniciar == false ? "Número da Sorte" : `${intervaloMsg}`}
                </h1>
@@ -101,7 +101,7 @@ export default function Home() {
                      {dataDificuldade.map((nivelDificuldade) => (
                         // eslint-disable-next-line react/jsx-key
                         <button
-                           className="bg-green-500 rounded-md h-8 hover:bg-green-600 text-white text-lg s380:w-16 s430:w-20 s430:text-base s530:w-28 s530:text-lg s1030:w-32"
+                           className="bg-green-500 border-2 border-black h-8 hover:bg-green-600 text-white text-lg s380:w-16 s430:w-20 s430:text-base s530:w-28 s530:text-lg s1030:w-32"
                            onClick={() =>
                               selecionaDificuldade(nivelDificuldade.index)
                            }
@@ -114,7 +114,7 @@ export default function Home() {
                   ""
                )}
                <button
-                  className="bg-green-500 rounded-md h-8 m-5 hover:bg-green-600 text-white text-center s380:w-16 s380:m-3 s430:w-20 s430:text-base s530:w-28 s530:text-lg s1030:w-32 s1030:m-6"
+                  className="bg-green-500 border-2 border-black h-8 m-5 hover:bg-green-600 text-white text-center s380:w-16 s380:m-3 s430:w-20 s430:text-base s530:w-28 s530:text-lg s1030:w-32 s1030:m-6"
                   onClick={() => {
                      setIniciar(!iniciar); // Inicializa a partida.
                      geradorNumeroAleatorio(); // Invoca a função de inicialização com a lógica de execução.
